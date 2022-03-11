4a342bd86abee6bb984dacb9f851e9c4b4894c08
Liang Sun <2281261651@qq.com>
2022-03-11 17:31:32 +0800

    abc

--- abc.java
+++ abc.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class abc {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("ni好");
+        List<String> list2 = new ArrayList<>();
+        list2 = list.stream().filter(x  -> x.contains("n"))
+                .collect(Collectors.toList());
+        System.out.println(list2);
+    }
 }
